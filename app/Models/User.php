71209b320cacc7708f37c2d8f2b99a4bc4bace00
Brian Mutinda <mutindabrian26@gmail.com>
2021-01-07 16:14:52 +0300

    create databse seeder and api guard

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasRoles;
+    protected $guard_name = "api";
 
     /**
      * The attributes that are mass assignable.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,68 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // Default Roles
+        $apiStudentRole = Role::create(['name'=>'student', 'guard_name'=>'api']);
+        $webStudentRole = Role::create(['name'=>'student', 'guard_name'=>'web']);
+        $lecturerRole = Role::create(['name'=>'lecturer', 'guard_name'=>'api']);
+
+        // Default permissions
+        $permissions = [
+            ['name' => 'add course', 'guard_name'=>'api'],
+            ['name' => 'view courses api', 'guard_name'=>'api'],
+            ['name' => 'view courses', 'guard_name'=>'web'],
+            ['name' => 'add unit', 'guard_name'=>'api'],
+            ['name' => 'view unit', 'guard_name'=>'api'],
+            ['name' => 'edit unit'],
+            ['name' => 'delete unit'],
+            ['name' => 'view units', 'guard_name'=>'web'],
+            ['name' => 'view lecturers', 'guard_name'=>'web']
+        ];
+
+       collect($permissions)->each(function ($item){
+        Permission::create($item);
+       });
+
+        // role permission
+        $apiStudentRole->givePermissionTo(['view courses api', 'view unit', 'add unit']);
+        $webStudentRole->givePermissionTo(['view units', 'view lecturers']);
+        $lecturerRole->givePermissionTo(['add unit','view unit']);
+        $apiStudentRole->givePermissionTo('add course');
+
+        // users
+        $webStudent = User::create([
+            'name'=>'Web student',
+            'email'=>'webStudent@gmail.com',
+            'password'=>'12345',
+            //'guard_name'=>'web'
+        ]);
+
+        $apiStudent = User::create([
+            'name'=>'APi student',
+            'email'=>'apiStudent@gmail.com',
+            'password'=>'12345'
+        ]);
+
+        $lecturer = User::create([
+            'name'=>'Lecturer',
+            'email'=>'lecturer@gmail.com',
+            'password'=>'12345'
+        ]);
+
+        $admin = User::create([
+            'name'=>'Admin',
+            'email'=>'admin@gmail.com',
+            'password'=>'12345'
+        ]);
+
+        // assign role
+        // $webStudent->assignRole($webStudentRole);
+        $apiStudent->assignRole($apiStudentRole);
+        $lecturer->assignRole($lecturerRole);
+
+        // assign specific role
+        $apiStudent->givePermissionTo('add course');
+        // $webStudent->givePermissionTo('view lecturers');
     }
 }
